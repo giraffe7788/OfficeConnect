4936d61d5d291e7af352337f6649db5376965f42
PC-23 <PC-23@DESKTOP-4OSK6AA>
2023-09-07 15:25:02 +0900

    230907 1차 통합본

--- OfficeConnect/src/emp/controller/join.java
+++ OfficeConnect/src/emp/controller/join.java
@@ -10,18 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/join.do")
 public class join extends HttpServlet {
-	
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		//여기서 vo 만들고 그 vo를 서비스로 넘겨줌
-		//여기서 사이트 넘어가는 방식을 
-		
-		
-		
+
+		// 여기서 vo 만들고 그 vo를 서비스로 넘겨줌
+		// 여기서 사이트 넘어가는 방식을
+
 	}
+}
